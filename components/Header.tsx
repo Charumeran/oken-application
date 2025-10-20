b6d8bcb673d7dab310600ad4973826eef0a77935
Charumeran <s.ryosei06@gmail.com>
2025-10-21 03:00:20 +0900

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -65,7 +65,7 @@ export default function Header() {
             className={`flex items-center ${pathname !== '/' ? 'cursor-pointer hover:opacity-80 transition-opacity' : ''}`}
             onClick={handleLogoClick}
           >
-            <Image src="/icons/icon.jpeg" alt="株式会社櫻建" width={240} height={240} />
+            <h1 className='text-black text-2xl font-bold'>建設テックパートナー</h1>
           </div>
           {pathname !== '/' && (
             <div className="flex items-center gap-2 md:gap-4">
