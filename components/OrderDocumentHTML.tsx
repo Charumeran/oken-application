3f5424bb1231c49f232f36fca86902c8666e7583
Charumeran <s.ryosei06@gmail.com>
2025-11-03 15:16:07 +0900

    縮小

--- components/OrderDocumentHTML.tsx
+++ components/OrderDocumentHTML.tsx
@@ -77,7 +77,7 @@ export const generatePDFContent = (data: OrderDocument, options?: { hidePrintBut
             width: 100% !important;
             max-width: 100% !important;
             overflow: hidden !important;
-            transform: scale(0.90);
+            transform: scale(0.85);
             transform-origin: center top;
           }
           .tables-container {
