c3eafd94ff57eb01d43fa2fb212115b3dee7568f
Charumeran <s.ryosei06@gmail.com>
2025-11-03 11:29:45 +0900

    headerをhidden, airprint対応のマージ

--- components/Header.tsx
+++ components/Header.tsx
@@ -58,7 +58,7 @@ export default function Header() {
   }
 
   return (
-    <header className="bg-white shadow">
+    <header className="print:hidden bg-white shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-6">
           <div 
